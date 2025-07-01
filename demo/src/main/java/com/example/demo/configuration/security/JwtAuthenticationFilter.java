5331ffd54e32a0ae5a014fa2d25df941de6b7a6b
doquoctuananh <71720140+doquoctuananh@users.noreply.github.com>
2025-07-01 16:37:33 +0700

    Jwt filter chain

--- demo/src/main/java/com/example/demo/configuration/security/JwtAuthenticationFilter.java
+++ demo/src/main/java/com/example/demo/configuration/security/JwtAuthenticationFilter.java
@@ -1,4 +1,57 @@
 package com.example.demo.configuration.security;
 
-public class JwtAuthenticationFilter {
+import com.example.demo.model.Jwt.JwtUtil;
+import com.example.demo.service.Implements.CustomUserDetailsService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    private UserDetailsService userDetailsService;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+
+        if(authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+
+        String token = authHeader.substring(7);
+        String userName = jwtUtil.getUserNameToken(token);
+        if(token != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+                UserDetails userDetails = userDetailsService.loadUserByUsername(userName);
+                if(jwtUtil.verifyToken(token,userName)) {
+                    UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                            userDetails, null, userDetails.getAuthorities()
+                    );
+                    authentication.setDetails(new WebAuthenticationDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+            }
+
+            filterChain.doFilter(request, response);
+    }
 }
