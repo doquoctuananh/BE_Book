4df7f39395d9d6f452808d8c623ba9c3952cb729
doquoctuananh <71720140+doquoctuananh@users.noreply.github.com>
2025-07-02 09:38:48 +0700

    controller home and login

--- demo/src/main/java/com/example/demo/controller/ControllerHome.java
+++ demo/src/main/java/com/example/demo/controller/ControllerHome.java
@@ -1,9 +1,20 @@
 package com.example.demo.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+//@RequestMapping("/")
 public class ControllerHome {
+    @GetMapping("/")
+    public ResponseEntity<String> home() {
+        return ResponseEntity.ok("Hello World");
+    }
+
+    @GetMapping("/api/home")
+    public ResponseEntity<String> ApiHome() {
+        return ResponseEntity.ok("Home Page");
+    }
 }
--- demo/src/main/java/com/example/demo/controller/ControllerLogin.java
+++ demo/src/main/java/com/example/demo/controller/ControllerLogin.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/api/auth/")
 public class ControllerLogin {
     @Autowired
     private AuthenticationManager authenticationManager;
