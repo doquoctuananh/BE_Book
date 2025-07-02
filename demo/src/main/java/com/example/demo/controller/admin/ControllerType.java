520ecdae29b5c2ad3c1bc665482e1e626bdb3965
doquoctuananh <71720140+doquoctuananh@users.noreply.github.com>
2025-07-02 14:07:50 +0700

    edit delete create find with Type

---
+++ demo/src/main/java/com/example/demo/controller/admin/ControllerType.java
@@ -0,0 +1,84 @@
+package com.example.demo.controller.admin;
+
+import com.example.demo.model.DTO.DTOType;
+import com.example.demo.model.Type;
+import com.example.demo.service.Implements.ImplServiceType;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/dashboard/type")
+public class ControllerType {
+
+    @Autowired
+    private ImplServiceType serviceType;
+
+    @GetMapping("")
+    @Transactional
+    public ResponseEntity<List<Type>> getAll() {
+        List<Type> listType = new ArrayList<>();
+        listType = serviceType.findAllTypes();
+        return ResponseEntity.ok().body(listType);
+    }
+
+
+    @PostMapping ("/create")
+    @Transactional
+    public ResponseEntity<?> createType(@Valid @RequestBody DTOType dtoType,
+                                        BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return ResponseEntity.badRequest().body(bindingResult.getFieldError().getDefaultMessage());
+        }
+        Type type = new Type();
+        BeanUtils.copyProperties(dtoType, type);
+        serviceType.saveType(type);
+        return ResponseEntity.ok().body(type);
+    }
+
+    @Transactional
+    @PatchMapping("edit/{id}")
+    public ResponseEntity<?> editType(@PathVariable("id") int id,
+                                      @Valid @RequestBody DTOType newDtoType,
+                                      BindingResult bindingResult) {
+//        System.out.println(id);
+        if(bindingResult.hasErrors()) {
+            return ResponseEntity.badRequest().body(bindingResult.getFieldError().getDefaultMessage());
+        }
+        Type type = serviceType.findTypeById(id);
+        if (type == null) {
+            return ResponseEntity.badRequest().body("Not found type with id " + id);
+        }
+        type.setTypeName(newDtoType.getTypeName());
+        serviceType.saveType(type);
+        return ResponseEntity.ok().body(type);
+    }
+
+    @Transactional
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getTypeWithId(@PathVariable (name ="id") int id) {
+        Type type = serviceType.findTypeById(id);
+        if (type == null) {
+            return ResponseEntity.badRequest().body("Not found type with id " + id);
+        }
+        return ResponseEntity.ok().body(type);
+    }
+
+    @Transactional
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteType(@PathVariable("id") int typeId){
+        Type type = serviceType.findTypeById(typeId);
+        if (type == null) {
+            return ResponseEntity.badRequest().body("Not found type with id " + typeId);
+        }
+        serviceType.deleteTypeById(typeId);
+        return ResponseEntity.ok().body("Delete successfully" + type);
+    }
+}
