30362cf36bae7899024428b8a5487722b98bc724
doquoctuananh <71720140+doquoctuananh@users.noreply.github.com>
2025-07-02 10:35:20 +0700

    push controller admin and user

--- demo/src/main/java/com/example/demo/controller/user/ControllerBook.java
+++ demo/src/main/java/com/example/demo/controller/user/ControllerBook.java
@@ -1,4 +1,15 @@
 package com.example.demo.controller.user;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/books")
 public class ControllerBook {
+    @GetMapping("/")
+    public ResponseEntity<String> getBooks() {
+        return ResponseEntity.ok().body("book page");
+    }
 }
