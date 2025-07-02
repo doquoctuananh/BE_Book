5387a46a412760f415e042494b31ead85b974be4
doquoctuananh <71720140+doquoctuananh@users.noreply.github.com>
2025-07-02 09:23:43 +0700

    reposnse token and authenication login

---
+++ demo/src/main/java/com/example/demo/model/Jwt/JwtResponse.java
@@ -0,0 +1,14 @@
+package com.example.demo.model.Jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class JwtResponse {
+    private String token;
+
+
+}
---
+++ demo/src/main/java/com/example/demo/model/Jwt/LoginRequest.java
@@ -0,0 +1,13 @@
+package com.example.demo.model.Jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class LoginRequest {
+    private String username;
+    private String password;
+}
